716c3f30381509a82ebebf059162b8943068d52b
saimm9546 <115878493+saimm9546@users.noreply.github.com>
2023-01-22 19:34:27 +0500

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,10 @@
+var email = document.getElementById('inpmail')
+
+
+function submit(){
+     if (email.value== '') {
+         alert("Please Enter Email For Subscribe")
+    } else {
+        alert("Thankyou For Subscription")
+    }
+}
